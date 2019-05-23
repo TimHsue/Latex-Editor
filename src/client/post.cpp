0cc8dbed4828457a416827b499e901a92bd5f778
TimHsue <TimHsue@gmail.com>
2019-05-23 11:40:19 +0800

    forget to update src

--- src/client/post.cpp
+++ src/client/post.cpp
@@ -5,13 +5,39 @@
 
 #define RQS_PACKAGE 1024
 #define RSP_PACKAGE 2048
+#define BOUNDARY "TPafZ#C3T"
 
-int main() {
-    SOCKET clientSocket;
+char *readFile(char *path) {
+	FILE* file = fopen(path, "r");
+	int fileLength = 0;
+	
+	if (not file) {
+		ERR("failed to open file!");
+	} else {
+		LOG("file opened.");
+	}
+	
+	fseek(file, 0, SEEK_END);
+	fileLength = ftell(file);
+	rewind(file);
+	
+	char *fileBuff = new char[fileLength + 1];
+	memset(fileBuff, 0, sizeof(fileBuff));
+	fread(fileBuff, sizeof(char), fileLength, file);
+	
+	fclose(file);
+	return fileBuff;
+}
+
+void postFile(char *addr, int serverPort, char *target, char *filePath) {
+	SOCKET clientSocket;
     WSADATA wsaData;
     struct sockaddr_in serverAddr;
 
+	string header;
     char sndBuff[RQS_PACKAGE], rcvBuff[RSP_PACKAGE];
+    memset(&sndBuff, 0, sizeof(sndBuff));
+    memset(&rcvBuff, 0, sizeof(rcvBuff));
 
     WSAStartup(MAKEWORD(2, 1), &wsaData);
 
@@ -23,8 +49,8 @@ int main() {
 
     memset(&serverAddr, 0, sizeof(serverAddr));
     serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(8888);
-    serverAddr.sin_addr.S_un.S_addr = inet_addr("106.52.251.85");
+    serverAddr.sin_port = htons(serverPort);
+    serverAddr.sin_addr.S_un.S_addr = inet_addr(addr);
 
     if (connect(clientSocket, (struct sockaddr *)&serverAddr,
                 sizeof(serverAddr)) < 0) {
@@ -32,29 +58,59 @@ int main() {
     } else {
         LOG("socket connected.");
     }
-
-    memset(rcvBuff, 0, sizeof(rcvBuff));
-    memset(sndBuff, 0, sizeof(sndBuff));
-
-    strcat(sndBuff, "POST http://106.52.251.85:8888/ HTTP/1.0\r\n");
-    strcat(sndBuff, "Host: 106.52.251.85:8888\r\n");
-    strcat(sndBuff, "Connection: keep-alive\r\n");
-    strcat(sndBuff, "\r\n");
-    LOG(sndBuff);
+    
+    char *fileContent = readFile(filePath);
+    int fileLength = strlen(fileContent);
+    
+    header = "";
+    header += "POST http://";
+    header += addr;
+	header += ":";
+	header += to_string(serverPort);
+	header += "?";
+    header += target;
+    header += " HTTP/1.0\r\n";
+    
+    header += "Host: ";
+    header += addr;
+	header += ":";
+	header += to_string(serverPort);
+    header += "\r\n";
+    
+    header += "Connection: keep-alive\r\n";
+    
+    header += "Accept: */*\r\n";
+    
+    header += "Content-Type: file\r\n";
+    header += "\r\n";
+    
+    header += "dataSize=";
+	header += to_string(fileLength);
+	header += "\r\n";
+	
+    LOG(header);
 
 	int reciveLength = 0;
-    if ((reciveLength = send(clientSocket, sndBuff, RQS_PACKAGE, 0)) < 0) {
-        ERR("failed to send request!");
+    if ((reciveLength = send(clientSocket, header.c_str(), header.length(), 0)) < 0) {
+        ERR("failed to send request header!");
+    } else {
+        LOG("request sent.");
+    }
+    
+	if ((reciveLength = send(clientSocket, fileContent, fileLength, 0)) < 0) {
+        ERR("failed to send request body!");
     } else {
         LOG("request sent.");
     }
 
     do {
+    	FILE* file = fopen("tmp.html", "w");
         if ((reciveLength = recv(clientSocket, rcvBuff, RSP_PACKAGE, 0)) <
             0) {
             ERR("failed to receive response!");
         } else if (reciveLength > 0) {
         	printf("%s", rcvBuff);
+        	fputs(rcvBuff, file);
         	memset(rcvBuff, 0, sizeof(rcvBuff));
         }
     } while (reciveLength > 0);
@@ -64,5 +120,10 @@ int main() {
     
     closesocket(clientSocket);
     WSACleanup();
+}
+
+int main() {
+	postFile("106.52.251.85", 8888, "texToHtml", "test.tex");
+	 
     return 0;
 }
